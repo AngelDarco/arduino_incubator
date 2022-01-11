be012a861c40dcc913e5e9824baae0bd4874bf43
angeldarco <angel92-eme@hotmail.com>
2022-01-11 18:50:48 +0000

    Eslint add

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,5 @@ import {Header} from './header/header.tsx'
 export const App = ()=>{
     return <div className="main__container">
         <Header/>
-
     </div>
 }
--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,55 +1,55 @@
 import logo from '../assets/img/icono.webp';
 import "./style.scss";
 
-export const Header = ()=>{
-    return<>
-    <div className="container" >
+export const Header = () => {
+    return <>
+        <div className="container" >
 
-        <div className="logo">
-            <img src={logo} alt="D"/>
+            <div className="logo">
+                <img src={logo} alt="D" />
+            </div>
+
+            <div className="title">
+                <h1>Darco Corporation</h1>
+            </div>
+
+            <div className="links">
+                <ul className='ul__pages'>
+                    <ul className="pages">Css
+                        <ul className="link" >
+                            <a href="http://darcocorporation.scienceontheweb.net/pages/css/book_cover/index.html">Book Cover</a>
+                            <a href="http://darcocorporation.scienceontheweb.net/pages/css/climbing_cube/index.html">Climbing Cube</a>
+                            <a href="http://darcocorporation.scienceontheweb.net/pages/css/cursor_effect/index.html">Cursor Effect</a>
+                        </ul>
+                    </ul>
+
+                    <ul className="pages">Js
+
+                    </ul>
+
+                    <ul className="pages">Html
+
+                    </ul>
+
+                    <ul className="pages">Php
+
+                    </ul>
+
+                    <ul className="pages">React
+
+                    </ul>
+
+                </ul>
+            </div>
+
         </div>
 
-        <div className="title">
-            <h1>Darco Corporation</h1>
-        </div>
-
-        <div className="links">
-        <ul className='ul__pages'>
-            <ul className="pages">Css
-                <ul className="link" >
-                    <a href="http://darcocorporation.scienceontheweb.net/pages/css/book_cover/index.html">Book Cover</a>
-                    <a href="http://darcocorporation.scienceontheweb.net/pages/css/climbing_cube/index.html">Climbing Cube</a>
-                    <a href="http://darcocorporation.scienceontheweb.net/pages/css/cursor_effect/index.html">Cursor Effect</a>
-                </ul>
-            </ul>
-
-                <ul className="pages">Js
-                
-                </ul>
-                
-                <ul className="pages">Html
-                
-                </ul>
-                
-                <ul className="pages">Php
-                
-                </ul>
-                
-                <ul className="pages">React
-                
-                </ul>
-
-        </ul>
-        </div>
-        
-    </div>
-    
     </>
 }
 
-function visivility(){
+function visivility() {
     let title = document.querySelectorAll('.pages');
-    for(let i in title){
+    for (let i in title) {
         console.log(i);
     }
 }
