0ddc640ef8a6043aab90b90621295e3f1b799a0c
angeldarco <angel92-eme@hotmail.com>
2022-02-05 03:56:29 +0000

    library Swiper add

--- src/pages/css/Css.tsx
+++ src/pages/css/Css.tsx
@@ -1,21 +1,22 @@
 import './Css.scss';
+import {SwiperSlide, Swiper} from 'swiper/react';
 import {CssImages, CssLinks } from '../../assets/videos/css/Images';
+import 'swiper/css';
 
 const Css = () => {
     return (
-            <div className="containerCss">
-                <div className="containerSlider">
-                    <div className="card"><img src={CssImages.book} alt="" onClick={()=>translate(CssLinks.book)} /></div>
-                    <div className="card"><img src={CssImages.card} alt="" onClick={()=>translate(CssLinks.card)}/></div>
-                    <div className="card"><img src={CssImages.cube} alt="" onClick={()=>translate(CssLinks.cube)}/></div>
-                    <div className="card"><img src={CssImages.earth} alt="" onClick={()=>translate(CssLinks.earth)}/></div>
-                    <div className="card"><img src={CssImages.loader} alt="" onClick={()=>translate(CssLinks.loader)}/></div>
-                </div>
-            </div>                   
+                  <Swiper className="containerCss">
+                   <SwiperSlide className="card"> <img src={CssImages.book} alt="" onClick={()=>translate(CssLinks.book)}/>   </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.card} alt="" onClick={()=>translate(CssLinks.card)}/>    </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.cube} alt="" onClick={()=>translate(CssLinks.cube)}/>    </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.earth} alt="" onClick={()=>translate(CssLinks.earth)}/>  </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.loader} alt="" onClick={()=>translate(CssLinks.loader)}/></SwiperSlide>
+                   </Swiper>
     )
 }
 function translate(name:string):void{
     window.open(name);
 }
+
 
 export default Css;
