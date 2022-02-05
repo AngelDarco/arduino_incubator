31414351b03128debfd4ca348733b53645b28935
angeldarco <angel92-eme@hotmail.com>
2022-02-05 05:29:25 +0000

    Icons add

--- src/pages/css/Css.tsx
+++ src/pages/css/Css.tsx
@@ -1,22 +1,26 @@
 import './Css.scss';
+import 'swiper/css';
 import {SwiperSlide, Swiper} from 'swiper/react';
 import {CssImages, CssLinks } from '../../assets/videos/css/Images';
-import 'swiper/css';
+import {AiOutlineArrowLeft,AiOutlineArrowRight} from 'react-icons/ai';
+import {BsFillPlayFill} from 'react-icons/bs';
 
 const Css = () => {
     return (
                   <Swiper className="containerCss">
-                   <SwiperSlide className="card"> <img src={CssImages.book} alt="" onClick={()=>translate(CssLinks.book)}/>   </SwiperSlide>
-                   <SwiperSlide className="card"> <img src={CssImages.card} alt="" onClick={()=>translate(CssLinks.card)}/>    </SwiperSlide>
-                   <SwiperSlide className="card"> <img src={CssImages.cube} alt="" onClick={()=>translate(CssLinks.cube)}/>    </SwiperSlide>
-                   <SwiperSlide className="card"> <img src={CssImages.earth} alt="" onClick={()=>translate(CssLinks.earth)}/>  </SwiperSlide>
-                   <SwiperSlide className="card"> <img src={CssImages.loader} alt="" onClick={()=>translate(CssLinks.loader)}/></SwiperSlide>
+                    <AiOutlineArrowLeft className='icon'/>
+                   <SwiperSlide className="card"> <img src={CssImages.book} alt="" onClick={()=>translate(CssLinks.book)}/> <BsFillPlayFill className="icon play"   onClick={()=>translate(CssLinks.book)}/>  </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.card} alt="" onClick={()=>translate(CssLinks.card)}/>  <BsFillPlayFill className="icon play"  onClick={()=>translate(CssLinks.card)} />   </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.cube} alt="" onClick={()=>translate(CssLinks.cube)}/>  <BsFillPlayFill className="icon play"  onClick={()=>translate(CssLinks.cube)} />   </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.earth} alt="" onClick={()=>translate(CssLinks.earth)}/> <BsFillPlayFill className="icon play"  onClick={()=>translate(CssLinks.earth)}/>  </SwiperSlide>
+                   <SwiperSlide className="card"> <img src={CssImages.loader} alt="" onClick={()=>translate(CssLinks.loader)}/> <BsFillPlayFill className="icon play"  onClick={()=>translate(CssLinks.loader)}/></SwiperSlide>
+                   <AiOutlineArrowRight className='icon right'/>
                    </Swiper>
-    )
+    )   
 }
 function translate(name:string):void{
     window.open(name);
 }
 
-
 export default Css;
+
